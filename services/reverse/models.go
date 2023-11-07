23eb5baa771e99d1ee4e0b7fad3bb3643075ed3d
farzin <farzin.nasiri@snapp.cab>
2023-11-07 15:18:10 +0330

    new models to support batch functionality

--- services/reverse/models.go
+++ services/reverse/models.go
@@ -13,3 +13,44 @@ type FrequentAddress struct {
 	Shortname        string `json:"shortname"`
 	EnglishShortname string `json:"shortname_en"`
 }
+
+// Request is the struct containing data on reverse request params
+type Request struct {
+	Type     ResponseType `json:"type"`
+	Display  string       `json:"display"`
+	Language Language     `json:"language"`
+	Zoom     float64      `json:"zoom"`
+	Lat      float64      `json:"lat"`
+	Lon      float64      `json:"lon"`
+	ID       int32        `json:"id"`
+}
+
+type BatchReverseRequest struct {
+	Requests []Request `json:"requests"`
+}
+
+type Result struct {
+	Result Components `json:"result"`
+	ID     int        `json:"id"`
+}
+
+type ResultWithDisplayName struct {
+	DisplayName DisplayName `json:"result"`
+	ID          int         `json:"id"`
+}
+
+type DisplayName struct {
+	DisplayName string `json:"displayName"`
+}
+
+type Components struct {
+	Components []Component `json:"components"`
+}
+
+type Results struct {
+	Results []Result `json:"results"`
+}
+
+type ResultsWithDisplayName struct {
+	Results []ResultWithDisplayName `json:"results"`
+}
