7a6574548363ccfe5926c713fe29b21c4aae33e5
farzin <farzin.nasiri@snapp.cab>
2023-11-07 15:17:56 +0330

    batch reverse functions added

---
+++ services/reverse/reverse_batch.go
@@ -0,0 +1,192 @@
+package reverse
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gitlab.snapp.ir/Map/sdk/smapp-sdk-go/config"
+	"gitlab.snapp.ir/Map/sdk/smapp-sdk-go/version"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+// GetBatch , receives a slice of  Request s and returns Component s of address of location given.
+// Does not support type 'frequent' in requests and Does not support type Display option as True
+func (c *Client) GetBatch(request BatchReverseRequest) ([]Result, error) {
+	return c.GetBatchWithContext(context.Background(), request)
+}
+
+// GetBatchWithContext is like GetBatch, but with context.Context support.
+// Does not support type 'frequent' in requests and Does not support type Display option as True
+func (c *Client) GetBatchWithContext(ctx context.Context, request BatchReverseRequest) ([]Result, error) {
+	if ctx == nil {
+		return nil, fmt.Errorf("smapp reverse geo-code: nil context")
+	}
+	// Start of parent span
+	var span trace.Span
+	ctx, span = otel.Tracer(c.tracerName).Start(ctx, "get-batch-reverse")
+	defer span.End()
+
+	var reqInitSpan trace.Span
+	ctx, reqInitSpan = otel.Tracer(c.tracerName).Start(ctx, "request-initialization")
+
+	jsonBody, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		reqInitSpan.RecordError(err)
+		reqInitSpan.End()
+		return nil, errors.New("smapp batch reverse geo-code: could not create request. err: " + err.Error())
+	}
+
+	params := url.Values{}
+
+	if c.cfg.APIKeySource == config.HeaderSource {
+		req.Header.Set(c.cfg.APIKeyName, c.cfg.APIKey)
+	} else if c.cfg.APIKeySource == config.QueryParamSource {
+		params.Set(c.cfg.APIKeyName, c.cfg.APIKey)
+	} else {
+		reqInitSpan.SetStatus(codes.Error, "invalid api key source")
+		reqInitSpan.End()
+		return nil, fmt.Errorf("smapp batch reverse geo-code: invalid api key source: %s", string(c.cfg.APIKeySource))
+	}
+
+	req.Header.Set(version.UserAgentHeader, version.GetUserAgent())
+
+	req.URL.RawQuery = params.Encode()
+
+	reqInitSpan.End()
+
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("smapp batch reverse geo-code: could not make a request due to this error: %s", err.Error())
+	}
+
+	//nolint
+	var responseSpan trace.Span
+	//nolint
+	ctx, responseSpan = otel.Tracer(c.tracerName).Start(ctx, "response-deserialization")
+
+	defer func() {
+		_, _ = io.Copy(ioutil.Discard, response.Body)
+		_ = response.Body.Close()
+	}()
+
+	var results Results
+	if response.StatusCode == http.StatusOK {
+		err := json.NewDecoder(response.Body).Decode(&results)
+		if err != nil {
+			responseSpan.RecordError(err)
+			responseSpan.End()
+			return nil, fmt.Errorf("smapp batch reverse geo-code: could not serialize response due to: %s", err.Error())
+		}
+		responseSpan.End()
+		return results.Results, nil
+	}
+
+	responseSpan.SetStatus(codes.Error, "non 200 status code")
+	responseSpan.End()
+	return nil, fmt.Errorf("smapp batch reverse geo-code: non 200 status: %d", response.StatusCode)
+}
+
+// GetBatchDisplayName , receives a slice of  Request s and returns Component s of address of location given, with only the DisplayName
+// Only works when Display is true
+func (c *Client) GetBatchDisplayName(request BatchReverseRequest) ([]ResultWithDisplayName, error) {
+	return c.GetBatchDisplayNameWithContext(context.Background(), request)
+}
+
+// GetBatchDisplayNameWithContext is like GetBatchWithDisplayName, but with context.Context support.
+// Only works when Display is true
+func (c *Client) GetBatchDisplayNameWithContext(ctx context.Context, request BatchReverseRequest) ([]ResultWithDisplayName, error) {
+	if ctx == nil {
+		return nil, fmt.Errorf("smapp reverse geo-code: nil context")
+	}
+	// Start of parent span
+	var span trace.Span
+	ctx, span = otel.Tracer(c.tracerName).Start(ctx, "get-batch-reverse")
+	defer span.End()
+
+	var reqInitSpan trace.Span
+	ctx, reqInitSpan = otel.Tracer(c.tracerName).Start(ctx, "request-initialization")
+
+	jsonBody, err := json.Marshal(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(jsonBody))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		reqInitSpan.RecordError(err)
+		reqInitSpan.End()
+		return nil, errors.New("smapp batch reverse geo-code: could not create request. err: " + err.Error())
+	}
+
+	params := url.Values{}
+
+	if c.cfg.APIKeySource == config.HeaderSource {
+		req.Header.Set(c.cfg.APIKeyName, c.cfg.APIKey)
+	} else if c.cfg.APIKeySource == config.QueryParamSource {
+		params.Set(c.cfg.APIKeyName, c.cfg.APIKey)
+	} else {
+		reqInitSpan.SetStatus(codes.Error, "invalid api key source")
+		reqInitSpan.End()
+		return nil, fmt.Errorf("smapp batch reverse geo-code: invalid api key source: %s", string(c.cfg.APIKeySource))
+	}
+
+	req.Header.Set(version.UserAgentHeader, version.GetUserAgent())
+
+	req.URL.RawQuery = params.Encode()
+
+	reqInitSpan.End()
+
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("smapp batch reverse geo-code: could not make a request due to this error: %s", err.Error())
+	}
+
+	//nolint
+	var responseSpan trace.Span
+	//nolint
+	ctx, responseSpan = otel.Tracer(c.tracerName).Start(ctx, "response-deserialization")
+
+	defer func() {
+		_, _ = io.Copy(ioutil.Discard, response.Body)
+		_ = response.Body.Close()
+	}()
+
+	var results ResultsWithDisplayName
+
+	respDump, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(respDump))
+
+	if response.StatusCode == http.StatusOK {
+		err := json.NewDecoder(response.Body).Decode(&results)
+		if err != nil {
+			responseSpan.RecordError(err)
+			responseSpan.End()
+			return nil, fmt.Errorf("smapp batch reverse geo-code: could not serialize response due to: %s", err.Error())
+		}
+		responseSpan.End()
+		return results.Results, nil
+	}
+
+	responseSpan.SetStatus(codes.Error, "non 200 status code")
+	responseSpan.End()
+	return nil, fmt.Errorf("smapp batch reverse geo-code: non 200 status: %d", response.StatusCode)
+}
